d35a237d5c03e761153d63a175be289a405a4b79
A Samuel Pottinger <sam.pottinger@berkeley.edu>
2023-08-03 09:04:19 -0700

    Update col names on file_eol

--- sql/02_clean_inputs.sql
+++ sql/02_clean_inputs.sql
@@ -54,7 +54,7 @@ SELECT
     CAST(YEAR_2 AS INTEGER) AS year,
     lower(Region) AS region,
     lower(EOL) AS eol,
-    CAST(Prim_W_EOL AS REAL) AS eolMT
+    CAST(Real_EOL AS REAL) AS eolMT
 FROM
     file_eol;
 
