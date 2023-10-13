c9d1d25f6a75b404e022fd812bcb93c57a3db385
Sam Pottinger <sam.pottinger@berkeley.edu>
2023-10-13 16:24:25 -0700

    Work from 1990 for now.

--- tasks_project_template.py
+++ tasks_project_template.py
@@ -87,7 +87,7 @@ class ProjectRawTask(luigi.Task):
         database_loc = job_info['database']
         connection = sqlite3.connect(database_loc)
 
-        years_before = reversed(range(1950, 2007))
+        years_before = reversed(range(1990, 2007))
         years_after = range(2021, 2051)
         years = itertools.chain(years_before, years_after)
         for year in years:
@@ -563,7 +563,7 @@ class ApplyLifecycleTask(luigi.Task):
 
         table = self.get_table_name()
 
-        years = list(range(1950, 2051))
+        years = list(range(1990, 2051))
         regions = ['china', 'eu30', 'nafta', 'row']
 
         timeseries = dict(map(
