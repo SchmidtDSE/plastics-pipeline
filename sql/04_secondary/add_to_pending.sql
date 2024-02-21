16c413686375dec6757b47f87945555865a8f94d
A Samuel Pottinger <sam@gleap.org>
2024-02-21 21:48:47 +0000

    Fix combine primary and secondary consumption.

--- sql/04_secondary/add_to_pending.sql
+++ sql/04_secondary/add_to_pending.sql
@@ -1,7 +1,8 @@
-INSERT INTO consumption_secondary_pending (year, region, consumptionMT)
+INSERT INTO consumption_secondary_pending (year, region, majorMarketSector, consumptionMT)
 SELECT
     consumption_secondary.year AS year,
     consumption_secondary.region AS region,
+    consumption_secondary.majorMarketSector AS majorMarketSector,
     consumption_secondary.consumptionMT AS consumptionMT
 FROM
     consumption_secondary
